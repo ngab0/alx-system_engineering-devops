e53eb986a0ba6359048619ff416340356371d2d2
ngab0 <jackdonnily@gmail.com>
2022-08-23 19:16:39 +0200

    BugFix

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/env ruby
 # a Ruby script that accepts one argument and pass it to a regular expression matching method
-# The regular expression must match Holberton
+# The regular expression must match School
 puts ARGV[0].scan(/School/).join
